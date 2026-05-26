dcd1fc92a986f233501d89143d37431848968130
郭旭 <guoxu@dianzhong.com>
2026-05-26 19:28:11 +0800

    feat(home): add home page with all sections and styles

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,3 +1,19 @@
+<!-- src/views/HomeView/HomeView.vue -->
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue';
+import HeroSection from './components/HeroSection.vue';
+import HowToSection from './components/HowToSection.vue';
+import FeaturesSection from './components/FeaturesSection.vue';
+import FooterSection from './components/FooterSection.vue';
+import './style.less';
+</script>
+
 <template>
-  <div>首页</div>
+  <div>
+    <NavBar />
+    <HeroSection />
+    <HowToSection />
+    <FeaturesSection />
+    <FooterSection />
+  </div>
 </template>
