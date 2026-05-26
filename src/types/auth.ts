47f314d7566ce169d4bb06a7b59719d681865001
郭旭 <guoxu@dianzhong.com>
2026-05-26 18:47:13 +0800

    feat(auth): add auth types

---
+++ src/types/auth.ts
@@ -0,0 +1,16 @@
+// src/types/auth.ts
+export interface User {
+  id: string
+  name: string
+  phone: string
+}
+
+export interface LoginParams {
+  phone: string
+  password: string
+}
+
+export interface AuthResponse {
+  token: string
+  user: User
+}
